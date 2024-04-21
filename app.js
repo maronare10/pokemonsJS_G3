af1667c23d08ed4fd0ab8b84326df66370fac58c
Mariana Navarro <maronare10@gmail.com>
2024-04-21 12:15:01 -0500

    agregando imágenes pokemon

--- app.js
+++ app.js
@@ -2,11 +2,24 @@ let page = 1
 
 const fetchPokemons = async (page = 1) => {
   const limit = 9
-  const offset = (page -1)*limit
+  const offset = (page - 1) * limit
   const response = await fetch(`https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`)
   const data = await response.json()
   // console.log(data);
-  const dataResults = data.results
+  const dataResults = data.results.map((pokemon) => {
+    // 'url': 'https://pokeapi.co/api/v2/pokemon/1/'
+    // const id = pokemon.url.split('/')[6]
+    const id = pokemon.url.split('/').at(6)
+    // console.log(id);
+    const image = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${id}.svg`
+
+    // console.log(image);
+    return {
+      ...pokemon, //name, url
+      id,
+      image
+    }
+  })
   // console.log(dataResults);
   return dataResults
 }
@@ -23,12 +36,17 @@ const renderPokemons = (pokemons) => {
 
   pokemons.forEach((pokemon) => {
     // elements = elements + `<h2>${pokemon.name}</h2>`
-    elements += `<h2>${pokemon.name}</h2>`
+    elements += `
+      <article class='pokemons-item'>
+        <img src="${pokemon.image}" width='80' height='80'/>
+        <h2> #${pokemon.id} ${pokemon.name}</h2>
+      </article>
+    `
     // console.log(elements);
 
-   })
+  })
 
-   pokemonsList.innerHTML = elements
+  pokemonsList.innerHTML = elements
 
 }
 
@@ -38,7 +56,7 @@ const documentReady = async () => {
   const currentPage = document.getElementById('currentPage')
 
   nextPage.addEventListener('click', async () => {
-  // page = page + 1
+    // page = page + 1
     const pokemons = await fetchPokemons(++page)
     renderPokemons(pokemons)
     currentPage.innerHTML = page
@@ -57,7 +75,7 @@ const documentReady = async () => {
   renderPokemons(pokemons)
 
 }
-document.addEventListener('DOMContentLoaded', documentReady )
+document.addEventListener('DOMContentLoaded', documentReady)
 
 
 
